f659f105a0703ea053277d1326b600ac72541996
Kuei <ragnakuei@livemail.tw>
2023-03-13 21:15:07 +0800

    ok

---
+++ WindowsFormsApp2/BaseDialogA.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public class BaseDialogA : Form
+    {
+        private Button button1;
+
+        public BaseDialogA()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(92, 97);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // BaseDialogA
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Name = "BaseDialogA";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+
+
+二種組合
+
+- BaseDialogA 跟 DialogA
+  - DialogA 無法修改 button 的位置
+- BaseDialogB 跟 DialogB
+  - DialogA 可以修改 button 的位置
+  - 更改 BaseDialogB 之後，要重新編譯，DialogB 才會有變化
